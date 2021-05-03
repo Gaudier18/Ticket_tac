e2b2a1bb2b69ed4bb3767f9a25d3a6551f84ef32
Kohys58 <55973572+Kohys58@users.noreply.github.com>
2021-05-03 18:32:02 +0200

    Evol config api

--- API/database/db.conf.js
+++ API/database/db.conf.js
@@ -11,12 +11,13 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const BDD = process.env.BDD == undefined ? "ticket_tac" : process.env.BDD;
+const BDD_URL = process.env.BDD_URL == undefined ? "127.0.0.1" : process.env.BDD_URL;
 const UTILISATEUR = process.env.UTILISATEUR == undefined ? "postgres" : process.env.UTILISATEUR;
 const MOT_DE_PASSE = process.env.MOT_DE_PASSE == undefined ? "postgres" : process.env.MOT_DE_PASSE;
 
 // Connexion à la base de données et définition des propriétés par défaut pour les models
 const sequelize = new Sequelize(BDD, UTILISATEUR, MOT_DE_PASSE,{
-    host: "127.0.0.1",
+    host: BDD_URL,
     dialect: 'postgres',
     define: {
         timestamps: false,
